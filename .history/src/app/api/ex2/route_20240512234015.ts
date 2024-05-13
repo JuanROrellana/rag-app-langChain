695feb9399c24871cd54b18ee6d007d2313c42fa
Juan Ramirez <ramirez.orellana.juanjose@gmail.com>
2024-05-12 23:43:59 -0600

    Adding history

---
+++ .history/src/app/api/ex2/route_20240512234015.ts
@@ -0,0 +1,26 @@
+import {
+  Message as VercelChatMessage,
+  StreamingTextResponse,
+  createStreamDataTransformer,
+} from "ai";
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { HttpResponseOutputParser } from "langchain/output_parsers";
+
+export const dynamic = "force-dynamic";
+
+/**
+ * Basic memory formatter that stringifies and passes
+ * message history directly into the model.
+ */
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
+
+const TEMPLATE = `You are a comedian. You have witty replies to user questions and you tell jokes.
+
+Current conversation:
+{chat_history}
+
+user: {input}
+assistant:`;
